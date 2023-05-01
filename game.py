c51ca15f2b308b84d7a3e67c9d2d7ab5c9f8b776
abdulkareemAbunabhan <zman17881@gmail.com>
2023-05-01 20:50:17 +0000

    something left

--- game.py
+++ game.py
@@ -1,7 +1,8 @@
 from game_logic import GameLogic
 roll_dice = GameLogic.roll_dice
 points_calculate = GameLogic.calculate_score
-def play (roller = GameLogic.roll_dice):
+validate_keepers=GameLogic.validate_keepers
+def play(roller=GameLogic.roll_dice):
     """
     The play() is responsible for starting the game.
     """
@@ -9,63 +10,89 @@ def play (roller = GameLogic.roll_dice):
     roll_dice = roller
     print("Welcome to Ten Thousand")
     print("(y)es to play or (n)o to decline")
-    user_response= input('> ')
-    if user_response == 'y' or user_response == 'Y':
+    user_response = input('> ')
+    if user_response.lower() == 'y':
         start_game()
-    if user_response == "n" or user_response == "N":
+    elif user_response.lower() == "n":
         quitter()
-def start_game(round_num=1,total=0,number_dices = 6,round_score=0):
-        """
-        This function starts the game once the user enters (y/Y).
-        """
-        print(f"Starting round {round_num}")
-        print(f"Rolling dice {number_dices}...")
-        first_roll=roll_dice(number_dices)
-        print(f"***{first_roll}***")
-        dice_picked=input('Enter dice to keep, or (q)uit:')
-        if(dice_picked == "q"):
-             end_game(total)
-             return
-        if(dice_picked is ""):
-             round_score=0
-             bank_points(round_score,round_num,total)
-        kept_dices = tuple(int(x) for x in dice_picked)
-        remaining_dices=number_dices-len(kept_dices)
-        if(not first_roll.__contains__(kept_dices)):
-            print("cheater!!!")
-            end_game(total)
-            return             
-        round_score+=points_calculate(kept_dices)
-        if(remaining_dices is 0):
-             bank_points(round_score,round_num,total)
-        print(f"You have {round_score} unbanked points and {remaining_dices} dice remaining")
-        print(f"(r)oll again, (b)ank your points or (q)uit:")
-        end_of_round=input("> ")
-        if(end_of_round is 'b'):
-             bank_points(round_score,round_num,total)
-        elif(end_of_round is 'r'):
-             if(remaining_dices>0):
-              start_game(round_num,total,remaining_dices,round_score)   
-        else:
-            end_game(total)       
-def quitter ():
-        """
-        This function quits the game.
-        """
-        return print('OK. Maybe another time')
-def bank_points(points,round_num,total):
+def start_game(round_num=1, total=0, number_dices=6, round_score=0):
     """
-    this function banks the points user earned whenever the user presses (b).
+    This function starts the game once the user enters (y/Y).
     """
-    total = total + points
+    if(number_dices == 6):
+     print(f"Starting round {round_num}")
+    print(f"Rolling {number_dices} dice...")
+    first_roll = roll_dice(number_dices)
+    result = ' '.join(str(x) for x in first_roll)
+    print(f"*** {result} ***")
+    print('Enter dice to keep, or (q)uit:')
+    dice_picked = input("> ")
+    if dice_picked.lower() == "q":
+        end_game(total)
+        return
+    if dice_picked == "":
+        round_score = 0
+        bank_points(round_score, round_num, total)
+    kept_dices = tuple(int(x) for x in dice_picked)
+    if not validate_keepers(first_roll,kept_dices):
+        print("Cheater!!! Or possibly made a typo...")
+        cheater(first_roll,round_num,round_score,total)
+    remaining_dices = number_dices - len(kept_dices)
+    round_score += points_calculate(kept_dices)
+    if remaining_dices == 0:
+        bank_points(round_score, round_num, total)
+    print(f"You have {round_score} unbanked points and {remaining_dices} dice remaining")
+    print(f"(r)oll again, (b)ank your points or (q)uit:")
+    end_of_round = input("> ")
+    if end_of_round.lower() == 'b':
+        bank_points(round_score, round_num, total)
+    elif end_of_round.lower() == 'r':
+        if (remaining_dices == 0 and number_dices == 6):
+            remaining_dices=6
+            start_game(round_num, total, remaining_dices, round_score)
+        elif remaining_dices > 0:
+            start_game(round_num, total, remaining_dices, round_score)
+    else:
+        end_game(total)
+def quitter():
+    """
+    This function quits the game.
+    """
+    print('OK. Maybe another time')
+def bank_points(points, round_num, total):
+    """
+    This function banks the points user earned whenever the user presses (b).
+    """
+    total += points
     print(f"You banked {points} points in round {round_num}")
     print(f"Total score is {total} points")
     round_num += 1
-    start_game(round_num,total)
+    start_game(round_num, total)
+def cheater(first_roll,round_num,round_score,total):
+    result = ' '.join(str(x) for x in first_roll)
+    print(f"*** {result} ***")
+    print('Enter dice to keep, or (q)uit:')
+    dice_picked = input("> ")
+    if dice_picked.lower() == "q":
+        end_game(total)
+        return
+    if dice_picked == "":
+        round_score = 0
+        bank_points(round_score, round_num, total)
+    var=dice_picked.replace(" ","")    
+    kept_dices = tuple(int(x) for x in var)
+    if not validate_keepers(first_roll,kept_dices):
+        print("Cheater!!! Or possibly made a typo...")
+        cheater(first_roll,round_num,round_score,total)
+    elif(points_calculate(kept_dices)==0):
+        return "zilch"
+    else:
+        return kept_dices    
+        
 def end_game(total):
-      """
-      This function prints a Good-bye message in addition to the total points earned.
-      """
-      print(f"Thanks for playing. You earned {total} points")
+    """
+    This function prints a Good-bye message in addition to the total points earned.
+    """
+    print(f"Thanks for playing. You earned {total} points")
 if __name__ == "__main__":
     play()
