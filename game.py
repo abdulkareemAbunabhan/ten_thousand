dab4d7f76241024304df5dddd74a31096681f6a5
abdulkareemAbunabhan <zman17881@gmail.com>
2023-05-02 07:23:39 +0000

    done lab 08 -hot_Dice-zilcher-else

--- game.py
+++ game.py
@@ -19,12 +19,15 @@ def start_game(round_num=1, total=0, number_dices=6, round_score=0):
     """
     This function starts the game once the user enters (y/Y).
     """
-    if(number_dices == 6):
+    if(number_dices == 6 and round_score == 0):
      print(f"Starting round {round_num}")
     print(f"Rolling {number_dices} dice...")
     first_roll = roll_dice(number_dices)
     result = ' '.join(str(x) for x in first_roll)
     print(f"*** {result} ***")
+    if(points_calculate(first_roll) == 0):
+        zilcher(round_num,total)
+        return
     print('Enter dice to keep, or (q)uit:')
     dice_picked = input("> ")
     if dice_picked.lower() == "q":
@@ -33,32 +36,35 @@ def start_game(round_num=1, total=0, number_dices=6, round_score=0):
     if dice_picked == "":
         round_score = 0
         bank_points(round_score, round_num, total)
-    kept_dices = tuple(int(x) for x in dice_picked)
+    var =dice_picked.replace(" ","")   
+    kept_dices = tuple(int(x) for x in var)
     if not validate_keepers(first_roll,kept_dices):
         print("Cheater!!! Or possibly made a typo...")
-        cheater(first_roll,round_num,round_score,total)
+        kept_dices=cheater(first_roll,round_num,round_score,total)
     remaining_dices = number_dices - len(kept_dices)
     round_score += points_calculate(kept_dices)
-    if remaining_dices == 0:
-        bank_points(round_score, round_num, total)
+    if (remaining_dices == 0 and number_dices == 6):
+        hot_dice(round_score,round_num,total,number_dices)
+        return
+    elif remaining_dices == 0:
+        bank_points(round_score,round_num,total)
+        return
     print(f"You have {round_score} unbanked points and {remaining_dices} dice remaining")
     print(f"(r)oll again, (b)ank your points or (q)uit:")
     end_of_round = input("> ")
     if end_of_round.lower() == 'b':
         bank_points(round_score, round_num, total)
     elif end_of_round.lower() == 'r':
-        if (remaining_dices == 0 and number_dices == 6):
-            remaining_dices=6
-            start_game(round_num, total, remaining_dices, round_score)
-        elif remaining_dices > 0:
             start_game(round_num, total, remaining_dices, round_score)
     else:
         end_game(total)
+
 def quitter():
     """
     This function quits the game.
     """
     print('OK. Maybe another time')
+
 def bank_points(points, round_num, total):
     """
     This function banks the points user earned whenever the user presses (b).
@@ -68,6 +74,7 @@ def bank_points(points, round_num, total):
     print(f"Total score is {total} points")
     round_num += 1
     start_game(round_num, total)
+
 def cheater(first_roll,round_num,round_score,total):
     result = ' '.join(str(x) for x in first_roll)
     print(f"*** {result} ***")
@@ -89,6 +96,25 @@ def cheater(first_roll,round_num,round_score,total):
     else:
         return kept_dices    
         
+def zilcher(round_num,total):
+    print("****************************************")
+    print("**        Zilch!!! Round over         **")
+    print("****************************************")
+    print(f"You banked 0 points in round {round_num}")
+    print(f"Total score is {total} points")
+    round_num+=1
+    start_game(round_num,total)
+
+def hot_dice(round_score,round_num,total,dices_number):
+    print(f"You have {round_score} unbanked points and {dices_number} dice remaining")
+    print(f"(r)oll again, (b)ank your points or (q)uit:")
+    end_of_round = input("> ")
+    if end_of_round.lower() == 'b':
+        bank_points(round_score, round_num, total)
+    elif end_of_round.lower() == 'r':
+            start_game(round_num, total, dices_number, round_score)
+    else:
+        end_game(total)
 def end_game(total):
     """
     This function prints a Good-bye message in addition to the total points earned.
